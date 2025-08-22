2dbbb5fe163c4369f5e582e93be0eb41bf57cb73
Edwin Piragauta <edwin.piragauta@gmail.com>
2025-08-21 22:34:05 -0500

    feat: Add route for PgnComparativaRegionalizacion component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -25,6 +25,7 @@ import { SgpInicioComponent } from './components/sgp-inicio/sgp-inicio.component
 import { SgpDetallePresupuestalComponent } from './components/sgp-detalle-presupuestal/sgp-detalle-presupuestal.component';
 import { SgpComparativaComponent } from './components/sgp-comparativa/sgp-comparativa.component';
 import { PgnRegionalizacionPresupuestoProgramacionComponent } from './components/pgn-regionalizacion-presupuesto-programacion/pgn-regionalizacion-presupuesto-programacion.component';
+import { PgnComparativaRegionalizacionComponent } from './components/pgn-comparativa-regionalizacion/pgn-comparativa-regionalizacion.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -52,7 +53,8 @@ export const routes: Routes = [
     { path: 'sgp-documentos-anexos', component: ReportsSgpComponent },
     { path: 'sgp-detalle-presupuestal', component: SgpDetallePresupuestalComponent },
     { path: 'sgp-comparativa', component: SgpComparativaComponent },
-    { path: 'pgn-regionalizacion', component: PgnRegionalizacionPresupuestoProgramacionComponent }
-
+    { path: 'pgn-regionalizacion', component: PgnRegionalizacionPresupuestoProgramacionComponent },
+    { path: 'pgn-comparativa-regionalizacion', component: PgnComparativaRegionalizacionComponent }
+    
 
 ];
